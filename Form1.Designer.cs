9e097082d2ad88b4ac5adcab1f6d2b3dd5b447ee
Huynh Thai Hoa <huynhlequang@gmail.com>
2022-03-31 23:56:11 +0900

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace Minesweeper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelCell = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbScore = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lbStatus = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbMode = new System.Windows.Forms.ComboBox();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panelCell
+            // 
+            this.panelCell.ColumnCount = 2;
+            this.panelCell.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.panelCell.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.panelCell.Location = new System.Drawing.Point(13, 75);
+            this.panelCell.Name = "panelCell";
+            this.panelCell.RowCount = 2;
+            this.panelCell.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.panelCell.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.panelCell.Size = new System.Drawing.Size(775, 600);
+            this.panelCell.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Score";
+            // 
+            // tbScore
+            // 
+            this.tbScore.Enabled = false;
+            this.tbScore.Location = new System.Drawing.Point(83, 25);
+            this.tbScore.Name = "tbScore";
+            this.tbScore.Size = new System.Drawing.Size(150, 25);
+            this.tbScore.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(556, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Time";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(616, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(150, 25);
+            this.textBox1.TabIndex = 4;
+            // 
+            // lbStatus
+            // 
+            this.lbStatus.AutoSize = true;
+            this.lbStatus.Location = new System.Drawing.Point(388, 29);
+            this.lbStatus.Name = "lbStatus";
+            this.lbStatus.Size = new System.Drawing.Size(0, 15);
+            this.lbStatus.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(12, 700);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Mode";
+            // 
+            // cbMode
+            // 
+            this.cbMode.FormattingEnabled = true;
+            this.cbMode.Items.AddRange(new object[] {
+            "9x9",
+            "16x16",
+            "30x16"});
+            this.cbMode.Location = new System.Drawing.Point(83, 700);
+            this.cbMode.Name = "cbMode";
+            this.cbMode.Size = new System.Drawing.Size(150, 23);
+            this.cbMode.TabIndex = 7;
+            this.cbMode.SelectedIndexChanged += new System.EventHandler(this.cbMode_SelectedIndexChanged);
+            // 
+            // btnNew
+            // 
+            this.btnNew.Font = new System.Drawing.Font("Gulim", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnNew.Location = new System.Drawing.Point(346, 694);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(115, 33);
+            this.btnNew.TabIndex = 8;
+            this.btnNew.Text = "New game";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 744);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.cbMode);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbStatus);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbScore);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panelCell);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel panelCell;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbScore;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lbStatus;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbMode;
+        private System.Windows.Forms.Button btnNew;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minesweeper
+{
+    public partial class Form1 : Form
+    {
+        //number of rows
+        int mRowCount;
+
+        //number of columns
+        int mColCount;
+
+        //number of cells
+        int mCellCount;
+
+        //number of mines
+        int mMineCount;
+
+        //matrix of cell value
+        int[,] mCellValue;
+
+        //matrix of cell button
+        Button[,] mCellButton;
+
+        public Form1()
+        {
+            InitializeComponent();
+            cbMode.SelectedIndex = 0;
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            panelCell.Controls.Clear();
+
+            panelCell.RowStyles.Clear();
+            panelCell.ColumnStyles.Clear();
+
+            panelCell.RowCount = mRowCount;
+            panelCell.ColumnCount = mColCount;
+
+            mCellButton = new Button[mRowCount, mColCount];
+            mCellValue = new int[mRowCount, mColCount];
+
+            for (int i = 0; i < mColCount; i++)
+            {
+                panelCell.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100 / mColCount));
+            }
+            for (int i = 0; i < mRowCount; i++)
+            {
+                panelCell.RowStyles.Add(new RowStyle(SizeType.Percent, 100 / mColCount));
+            }
+
+            for(int i = 0; i < mRowCount; i++)
+            {
+                for(int j = 0; j < mColCount; j++)
+                {
+                    mCellButton[i, j] = new Button();
+                    mCellButton[i, j].Dock = DockStyle.Fill;
+                    panelCell.Controls.Add(mCellButton[i, j], j, i);
+                }
+            }
+        }
+
+        private void cbMode_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch(cbMode.SelectedIndex)
+            {
+                case 0: //9X9 -> 10 mines
+                    {
+                        mRowCount = 9;
+                        mColCount = 9;
+                        mMineCount = 10;
+                        break;
+                    }
+                case 1: //16X16 -> 32 mines
+                    {
+                        mRowCount = 16;
+                        mColCount = 16;
+                        mMineCount = 32;
+                        break;
+                    }
+                case 2: //16X30 -> 60 mines
+                    {
+                        mRowCount = 16;
+                        mColCount = 30;
+                        mMineCount = 60;
+                        break;
+                    }
+            }
+
+            mCellCount = mRowCount * mColCount;
+        }
+
+
+        private List<Point> getMineList()
+        {
+            List<Point> mineList = new List<Point>();
+            Random rd = new Random();
+            while (mineList.Count < mMineCount)
+            {
+                int seed = rd.Next(0, mCellCount);
+                Point point = new Point(seed / mColCount, seed % mColCount);
+                if (!mineList.Contains(point))
+                    mineList.Add(point);
+            }
+            return mineList;
+        }
+
+        private void setCellValues()
+        {
+            List<Point> mineList = getMineList();
+
+        }
+    }
+}
