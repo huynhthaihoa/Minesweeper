93c45df923d0f3947543526d371de73fc86bfddc
Hoa <huynhlequang@gmail.com>
2022-04-01 17:52:35 +0900

    Add custom control CellButton to replace original Button (04/01/2022 - 17:52)

---
+++ CellButton.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Minesweeper
+{
+    public partial class CellButton : Button
+    {
+        private int mRow;
+
+        private int mCol;
+
+        //private int mVal;
+
+        public int Row { get { return mRow; } }
+
+        public int Col { get { return mCol; } }
+
+        //public int Value
+        //{
+        //    get { return mVal; }
+        //    set { mVal = value; }
+        //}
+
+        public CellButton()
+        {
+            InitializeComponent();
+        }
+
+        public CellButton(int row, int col)
+        {
+            mRow = row;
+            mCol = col;
+            //mVal = 0;
+            Dock = DockStyle.Fill;
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+        }
+    }
+}
